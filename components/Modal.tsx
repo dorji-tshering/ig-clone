6d1682f0c175e0bd16dc8521e9cbeed3e3419091
Jigthril Kinley Yoesel <dorjitshering370@gmail.com>
2022-12-11 16:24:52 +0600

    Added transition for the modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,4 +1,4 @@
-import { Dialog } from '@headlessui/react';
+import { Dialog, Transition } from '@headlessui/react';
 
 type Props = {
     open: boolean;
@@ -20,21 +20,32 @@ const Modal = ({open, onClose, children, bg="bg-white", overflowY=true}: Props) 
     
     return (
         <>
-            <Dialog 
-                open={open} 
-                onClose={onClose}
-                className="relative z-[999] shadow-searchShadow">
-                    {/* backdrop */}
-                <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-                <div className="fixed inset-0">
-                    <div className="flex h-full items-center justify-center p-4">
-                        <Dialog.Panel className={`mx-auto ${overflowY && 'overflow-y-auto'} w-full max-h-full
-                            sm:min-w-[400px] sm:w-auto rounded-lg ${bg} text-center`}>
-                            {children}
-                        </Dialog.Panel>
+            <Transition
+            show={open}
+            >
+                <Dialog 
+                    onClose={onClose}
+                    className="relative z-[999] shadow-searchShadow">
+                        {/* backdrop */}
+                    <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+                    <div className="fixed inset-0">
+                        <div className="flex h-full items-center justify-center p-4">
+                            <Transition.Child
+                                enter='ease-out duration-100'
+                                enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+                                enterTo='opacity-100 translate-y-0 sm:scale-100'
+                                leave='ease-in duration-100'
+                                leaveFrom='opacity-100 translate-y-0 sm:scale-100'
+                                leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'>
+                                <Dialog.Panel className={`mx-auto ${overflowY && 'overflow-y-auto'} w-full max-h-full
+                                    sm:min-w-[400px] sm:w-auto rounded-lg ${bg} text-center`}>
+                                    {children}
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
                     </div>
-                </div>
-            </Dialog>
+                </Dialog>
+            </Transition>  
         </>
     )
 }
