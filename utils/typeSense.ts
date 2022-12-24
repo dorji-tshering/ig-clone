1f3d4c9f9112f0cb98fc9d56c8e2c23022022955
Jigthril Kinley Yoesel <dorjitshering370@gmail.com>
2022-12-24 22:49:00 +0600

    Added search feature with typesense

--- utils/typeSense.ts
+++ utils/typeSense.ts
@@ -2,11 +2,11 @@ import Typesense from 'typesense'
 
 let client = new Typesense.Client({
     'nodes': [{
-      'host': '8f17dmzgv0sxio9bp-1.a1.typesense.net', 
+      'host': process.env.NEXT_PUBLIC_TYPESENSE_HOST as string, 
       'port': 443,
       'protocol': 'https'
     }],
-    'apiKey': 'hMmU3fNDJdiJPTOAVilAvYHLbcvUnvD9',
+    'apiKey': process.env.NEXT_PUBLIC_TYPESENSE_KEY as string,
     'connectionTimeoutSeconds': 2
 })
 //client.collections('companies').documents().upsert(document)
