aacc1305b123cfa744061ab840ed7e1ee362de0c
Jigthril Kinley Yoesel <dorjitshering370@gmail.com>
2022-12-28 19:51:20 +0600

    remove comments in firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -2,11 +2,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APIKEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
