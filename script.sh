652713a8e0ab1402c3d08afd93cf153fb7163dec
Jigthril Kinley Yoesel <dorjitshering370@gmail.com>
2022-12-29 21:50:05 +0600

    updated filter image filter logic

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
-  # Don't build
-  echo "🛑 - Build cancelled"
-  exit 0;
+# if [[ "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+#   # Don't build
+#   echo "🛑 - Build cancelled"
+#   exit 0;
 
-else
-  # Proceed with the build
-    echo "✅ - Build can proceed"
-  exit 1;
-fi
+# else
+#   # Proceed with the build
+#     echo "✅ - Build can proceed"
+#   exit 1;
+# fi
